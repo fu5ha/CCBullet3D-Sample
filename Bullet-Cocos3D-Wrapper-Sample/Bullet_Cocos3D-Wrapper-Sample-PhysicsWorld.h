e810cd6d049e70849dfa0a34cac68de3daaea6d5
Gray Olson <friends.gray@gmail.com>
2011-07-01 21:40:38 -0700

    global commit 2. v1.0. working

---
+++ Bullet-Cocos3D-Wrapper-Sample/Bullet_Cocos3D-Wrapper-Sample-PhysicsWorld.h
@@ -0,0 +1,16 @@
+//
+//  Bullet_Cocos3D-Wrapper-Sample-PhysicsWorld.h
+//  Bullet-Cocos3D-Wrapper-Sample
+//
+//  Created by Scot Olson on 7/1/11.
+//  Copyright 2011 Rhonda Graphics Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Bullet_Cocos3D_Wrapper_Sample_PhysicsWorld : CCNode {
+    
+}
+
+@end
---
+++ Bullet-Cocos3D-Wrapper-Sample/Bullet_Cocos3D-Wrapper-Sample-PhysicsWorld.mm
@@ -0,0 +1,14 @@
+//
+//  Bullet_Cocos3D-Wrapper-Sample-PhysicsWorld.mm
+//  Bullet-Cocos3D-Wrapper-Sample
+//
+//  Created by Scot Olson on 7/1/11.
+//  Copyright 2011 Rhonda Graphics Inc. All rights reserved.
+//
+
+#import "Bullet_Cocos3D-Wrapper-Sample-PhysicsWorld.h"
+
+
+@implementation Bullet_Cocos3D_Wrapper_Sample_PhysicsWorld
+
+@end

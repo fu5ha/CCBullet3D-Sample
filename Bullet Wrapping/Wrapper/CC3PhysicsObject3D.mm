09bcd065f549af8b0bb338997c31936a436ab763
Gray Olson <friends.gray@gmail.com>
2011-07-01 21:21:57 -0700

    version 0.2, working

---
+++ Bullet Wrapping/Wrapper/CC3PhysicsObject3D.mm
@@ -0,0 +1,96 @@
+/*
+ * CC3: http://isgl3d.com
+ *
+ * Copyright (c) 2010-2011 Stuart Caunt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+extern "C" {
+#import "CC3Foundation.h"
+};
+
+#import "CC3PhysicsObject3D.h"
+#import "CC3PhysicsWorld.h"
+#import "CC3Node.h"
+
+#import "btBulletDynamicsCommon.h"
+
+@implementation CC3PhysicsObject3D
+
+@synthesize node = _node;
+@synthesize rigidBody = _rigidBody;
+@synthesize shape = _shape;
+@synthesize isStatic;
+
+- (id) initWithNode:(CC3Node *)node andRigidBody:(btRigidBody *)rigidBody isStatic:(BOOL)isstatic {
+    if ((self = [super init])) {
+    	_node = [node retain];
+    	_rigidBody = rigidBody;
+        _shape = _rigidBody->getCollisionShape();
+        isStatic = isstatic;
+    	
+    }
+	
+    return self;
+}
+
+- (void) dealloc {
+	[_node release];
+	
+	delete _rigidBody->getMotionState();
+	delete _shape;
+	delete _rigidBody;
+    delete p2p;
+	[super dealloc];
+}
+
+- (void) applyForce:(CC3Vector)force withPosition:(CC3Vector)position {
+	btVector3 bodyForce(force.x, force.y, force.z);
+	btVector3 bodyPosition(position.x, position.y, position.z);
+
+	_rigidBody->applyForce(bodyForce, bodyPosition);	
+}
+
+-(void) applyImpulse:(CC3Vector)force withPosition:(CC3Vector)position {
+    btVector3 bodyForce(force.x, force.y, force.z);
+	btVector3 bodyPosition(position.x, position.y, position.z);
+    
+	_rigidBody->applyImpulse(bodyForce, bodyPosition);
+}
+
+- (void) setObjectLocation:(CC3Vector)position world:(CC3PhysicsWorld *)world {
+    btVector3 btPosition = btVector3(position.x, position.y, position.z);
+    p2p = new btPoint2PointConstraint(*_rigidBody, btVector3(_node.location.x, _node.location.y, _node.location.z));
+    world._discreteDynamicsWorld->addConstraint(p2p);
+    p2p->m_setting.m_impulseClamp = 30;
+    //very weak constraint for picking
+    p2p->m_setting.m_tau = 0.001f;
+    p2p->setPivotB(btPosition);
+}
+
+- (void) setRotationQuaternion:(CC3Vector4)quaternion{
+    btTransform nTrans;
+    _rigidBody->getMotionState()->getWorldTransform(nTrans);
+    btTransform rTrans;
+    rTrans = btTransform(btQuaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w), nTrans.getOrigin());
+    _rigidBody->getMotionState()->setWorldTransform(rTrans);
+}
+
+@end
---
+++ Bullet Wrapping/Wrapper/CC3PhysicsWorld.mm
@@ -0,0 +1,170 @@
+/*
+ * CC3: http://isgl3d.com
+ *
+ * Copyright (c) 2010-2011 Stuart Caunt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+extern "C" {
+#import "CC3Foundation.h"  
+};
+
+#import "CC3PhysicsWorld.h"
+#import "CC3PhysicsObject3D.h"
+#import "cocos2d.h"
+
+
+@implementation CC3PhysicsWorld
+@synthesize _discreteDynamicsWorld;
+
+- (id) init {
+    if ((self = [super init])) 
+	{
+    	
+    	_lastStepTime = [[NSDate alloc] init];
+       	_physicsObjects = [[NSMutableArray alloc] init];
+        broadphase = new btDbvtBroadphase();
+		collisionConfiguration = new btDefaultCollisionConfiguration();
+		dispatcher = new btCollisionDispatcher(collisionConfiguration);
+		solver = new btSequentialImpulseConstraintSolver();
+		dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
+		[self setDiscreteDynamicsWorld:dynamicsWorld];
+    }
+	
+    return self;
+}
+
+- (void) dealloc 
+{
+	
+	[_lastStepTime release];
+	[_physicsObjects release];
+    delete broadphase;
+    delete dynamicsWorld;
+    delete collisionConfiguration;
+    delete dispatcher;
+    delete solver;
+
+	[super dealloc];
+}
+
+- (void) setDiscreteDynamicsWorld:(btDiscreteDynamicsWorld *)discreteDynamicsWorld {
+	_discreteDynamicsWorld = discreteDynamicsWorld;
+}
+
+- (void) addPhysicsObject:(CC3PhysicsObject3D *)physicsObject {
+	
+	// Add collision object to dynamics world
+	_discreteDynamicsWorld->addRigidBody(physicsObject.rigidBody);
+	
+	// Add to physics list
+    if (!physicsObject.isStatic) {
+        [_physicsObjects addObject:physicsObject];
+    }
+	
+}
+- (void) removePhysicsObject:(CC3PhysicsObject3D *)physicsObject 
+{
+	// Remove from render list
+	[physicsObject.node remove];
+
+	// Remove collision object from dynamics world
+	_discreteDynamicsWorld->removeRigidBody(physicsObject.rigidBody);
+	
+	// Remove from physics list
+	[_physicsObjects removeObject:physicsObject];
+}
+
+- (void) removeAllChildren			
+{
+	[super  removeAllChildren];
+	
+	for (CC3PhysicsObject3D * physicsObject in _physicsObjects) {
+		_discreteDynamicsWorld->removeRigidBody(physicsObject.rigidBody);
+	}
+	
+	[_physicsObjects removeAllObjects];
+}
+
+- (void) synchTransformation {
+	// Get time since last step
+	NSDate * currentTime = [[NSDate alloc] init];
+	
+	NSTimeInterval timeInterval = [currentTime timeIntervalSinceDate:_lastStepTime];
+//	float optimalInterval = 1. / 60.;
+	
+//	if (timeInterval > optimalInterval) {
+//		timeInterval = optimalInterval;
+//	}
+	
+	// Update the simulation
+	_discreteDynamicsWorld->stepSimulation(timeInterval, 2);
+	[_lastStepTime release];
+	_lastStepTime = currentTime;
+
+	// Update all global matrices
+	for (CC3PhysicsObject3D *object in _physicsObjects) {
+        btTransform gTrans;
+        object.rigidBody->getMotionState()->getWorldTransform(gTrans);
+        btVector3 gPos = gTrans.getOrigin();
+        btVector3 axis = gTrans.getRotation().getAxis();
+        float angle = gTrans.getRotation().getAngle();
+        CC3Vector4 quaternion;
+        float sinAngle;
+        angle *= 0.5f;
+        axis = axis.normalized();
+        sinAngle = sin(angle);
+        quaternion.x = (axis.getX() * sinAngle);
+        quaternion.y = (axis.getY() * sinAngle);
+        quaternion.z = (axis.getZ() * sinAngle);
+        quaternion.w = cos(angle);
+        object.node.location = CC3VectorMake(gPos.getX(), gPos.getY(), gPos.getZ());
+        object.node.quaternion = quaternion;
+    }
+    
+}
+
+- (void) setGravity:(float)x y:(float)y z:(float)z {
+	_discreteDynamicsWorld->setGravity(btVector3(x, y, z));
+}
+
+- (CC3PhysicsObject3D *) createPhysicsObject:(CC3Node *)node shape:(btCollisionShape *)shape mass:(float)mass restitution:(float)restitution position:(CC3Vector)position {
+	// Create a motion state for the object
+	btDefaultMotionState* motionState = new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1), btVector3(position.x, position.y, position.z)));
+	
+	// Create a rigid body
+	btVector3 localInertia(0, 0, 0);
+	shape->calculateLocalInertia(mass, localInertia);
+	btRigidBody * rigidBody = new btRigidBody(mass, motionState, shape, localInertia);
+	rigidBody->setRestitution(restitution);
+	rigidBody->setActivationState(DISABLE_DEACTIVATION);
+
+	// Create a physics object and add it to the physics world
+    if (mass > 0) {
+        isstatic = NO;
+    } else {
+        isstatic = YES;
+	}
+    CC3PhysicsObject3D *physicsObject = [[CC3PhysicsObject3D alloc] initWithNode:node andRigidBody:rigidBody isStatic:isstatic];
+    [self addPhysicsObject:physicsObject];
+	return physicsObject;
+}
+
+@end
